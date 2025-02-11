39d217c8059500f8ce4b4c19e0582efda446e843
WRC <hello_wrc@outlook.com>
2025-02-11 11:05:16 +0800

    ci: 添加文档自动化构建脚本

--- tools/generate-docs.ps1
+++ tools/generate-docs.ps1
@@ -1,11 +1,15 @@
 param($ref)
 
+
 if (Test-Path -Path ./ClassIsland) {
     Remove-Item -Path ./ClassIsland -Recurse -Force
 }
 
 git clone --recursive git@github.com:ClassIsland/ClassIsland.git
 Set-Location .\ClassIsland
+if ($null -eq $ref) {
+    $ref = $(git tag --sort=-creatordate)[0]
+}
 git checkout $ref
 Set-Location ..
 
